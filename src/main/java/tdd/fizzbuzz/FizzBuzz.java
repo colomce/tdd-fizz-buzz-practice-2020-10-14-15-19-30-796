1febaf0003d86b80adbc28d3d1bbf34ff55452ac
Cedric Don Coloma <colomce@oocl.com>
2020-10-15 14:32:05 +0800

    [FizzBuzzWhiz][Cedric Coloma] feature: Return FizzWhizz when orderNumber is multiples of three and seven

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -9,8 +9,13 @@ public class FizzBuzz {
     private static final int MODULO_5 = 5;
 
     public String countOff(int orderNumber) {
+
         if (orderNumber % MODULO_3 == 0 && orderNumber % MODULO_5 == 0) {
             return FIZZ_BUZZ;
+        }
+
+        if (orderNumber % MODULO_3 == 0 && orderNumber % 7 == 0) {
+            return "FizzWhizz";
         }
 
         if (orderNumber % MODULO_3 == 0) {
