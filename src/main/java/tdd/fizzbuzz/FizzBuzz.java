1aa74f4a85e72c61e5d9d69c3cfc9c7e261ebb0d
Cedric Don Coloma <colomce@oocl.com>
2020-10-15 14:23:10 +0800

    [FizzBuzzWhiz][Cedric Coloma] refactor: Return FizzBuzz when number is multiples of three and five

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,12 +4,13 @@ public class FizzBuzz {
 
     private static final String FIZZ = "Fizz";
     private static final String BUZZ = "Buzz";
+    private static final String FIZZ_BUZZ = "FizzBuzz";
     private static final int MODULO_3 = 3;
     private static final int MODULO_5 = 5;
 
     public String countOff(int orderNumber) {
         if (orderNumber % MODULO_3 == 0 && orderNumber % MODULO_5 == 0) {
-            return "FizzBuzz";
+            return FIZZ_BUZZ;
         }
 
         if (orderNumber % MODULO_3 == 0) {
