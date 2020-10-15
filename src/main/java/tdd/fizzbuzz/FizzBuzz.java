7eb3c9be44a43fdde6bdb6ff2a8638daf8b87518
Cedric Don Coloma <colomce@oocl.com>
2020-10-15 14:17:44 +0800

    [FizzBuzzWhiz][Cedric Coloma] refactor: Extract Buzz to constant and int five

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,15 +3,17 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
     private static final int MODULO_3 = 3;
+    private static final int MODULO_5 = 5;
 
     public String countOff(int orderNumber) {
         if (orderNumber % MODULO_3 == 0) {
             return FIZZ;
         }
 
-        if(orderNumber % 5 == 0) {
-            return "Buzz";
+        if(orderNumber % MODULO_5 == 0) {
+            return BUZZ;
         }
         return String.valueOf(orderNumber);
     }
