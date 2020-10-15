ec327b07fe465de6c39e42829318f80357e0313e
Cedric Don Coloma <colomce@oocl.com>
2020-10-15 13:58:54 +0800

    [FizzBuzzWhiz][Cedric Coloma] test: Should return the same orderNumber given 1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,22 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void should_return_1_when_countOff_given_orderNumber_1() {
+        //Given
+        String orderNumber = "1";
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //When
+        String actual = fizzBuzz.countOff(orderNumber);
+
+        //Then
+        assertEquals("1", actual);
+    }
 }
+
