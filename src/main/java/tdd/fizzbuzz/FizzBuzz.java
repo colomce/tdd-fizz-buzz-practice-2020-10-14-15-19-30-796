539ba144c140196a763d5d1182150fea88eb40a7
Cedric Don Coloma <colomce@oocl.com>
2020-10-15 14:21:35 +0800

    [FizzBuzzWhiz][Cedric Coloma] feature: Return FizzBuzz when number is multiples of three and five

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -8,11 +8,15 @@ public class FizzBuzz {
     private static final int MODULO_5 = 5;
 
     public String countOff(int orderNumber) {
+        if (orderNumber % MODULO_3 == 0 && orderNumber % MODULO_5 == 0) {
+            return "FizzBuzz";
+        }
+
         if (orderNumber % MODULO_3 == 0) {
             return FIZZ;
         }
 
-        if(orderNumber % MODULO_5 == 0) {
+        if (orderNumber % MODULO_5 == 0) {
             return BUZZ;
         }
         return String.valueOf(orderNumber);
