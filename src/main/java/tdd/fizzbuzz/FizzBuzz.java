84fd8f626a343256553ea38b260660a853b47d21
Cedric Don Coloma <colomce@oocl.com>
2020-10-15 14:33:41 +0800

    [FizzBuzzWhiz][Cedric Coloma] refactor: Extracted 7 to MODULO7 and FizzWhizz to FIZZ_WHIZZ constant

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,8 +5,10 @@ public class FizzBuzz {
     private static final String FIZZ = "Fizz";
     private static final String BUZZ = "Buzz";
     private static final String FIZZ_BUZZ = "FizzBuzz";
+    private static final String FIZZ_WHIZZ = "FizzWhizz";
     private static final int MODULO_3 = 3;
     private static final int MODULO_5 = 5;
+    private static final int MODULO_7 = 7;
 
     public String countOff(int orderNumber) {
 
@@ -14,8 +16,8 @@ public class FizzBuzz {
             return FIZZ_BUZZ;
         }
 
-        if (orderNumber % MODULO_3 == 0 && orderNumber % 7 == 0) {
-            return "FizzWhizz";
+        if (orderNumber % MODULO_3 == 0 && orderNumber % MODULO_7 == 0) {
+            return FIZZ_WHIZZ;
         }
 
         if (orderNumber % MODULO_3 == 0) {
