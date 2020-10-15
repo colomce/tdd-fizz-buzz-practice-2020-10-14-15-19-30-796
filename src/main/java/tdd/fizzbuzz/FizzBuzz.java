b7ffeea7f33adfa0a34693291c4b9798ada0c8a6
Cedric Don Coloma <colomce@oocl.com>
2020-10-15 14:05:52 +0800

    [FizzBuzzWhiz][Cedric Coloma] test: Added implementation to display Fizz when number is multiples of three

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,11 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(String orderNumber) {
+        int number = Integer.parseInt(orderNumber);
+
+        if(number % 3 == 0) {
+            return "Fizz";
+        }
         return orderNumber;
     }
 }
